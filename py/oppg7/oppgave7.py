d164830bdd5bd7451bd78c20a844e36e49d044b2
klype <mikkelfossum@gmail.com>
2023-04-11 13:28:06 +0200

    encryption = :-1:

--- py/oppg7/oppgave7.py
+++ py/oppg7/oppgave7.py
@@ -1,46 +1,25 @@
-from cryptography.fernet import Fernet
-
-# thanks https://stackoverflow.com/questions/2490334/simple-way-to-encode-a-string-according-to-a-password
-def encrypt(message: bytes, key: bytes) -> bytes:
-    return Fernet(key).encrypt(message)
-
-def decrypt(token: bytes, key: bytes) -> bytes:
-    return Fernet(key).decrypt(token)
-
 # This function takes in a string, then opens
 # a file, then writes the string to it.
 # ----------------------------------------------
 # IF THE FILE DOES NOT WORK IT IS IMPORTANT TO 
 # RUN IT WITH THE PY/PYTHON COMMAND IN THE FOLDER IT IS.
 def write_to_file(string: str):
-    file = open("oppgave7.save", "ab") # Opens a file as an variable
-
-    key = open("oppgave7.key", "r").read()
-    print("Key:",key) # Debug
-    token = encrypt(bytes(string, encoding="utf8"), key)
-    file.write(token+"\n".encode()) # Writes the choosen string to the file
+    file = open("oppgave7.save", "a") # Opens a file as an variable
+    file.write(string+"\n") # Writes the choosen string to the file
     file.close # Closes the file
-
-def get_file_content():
-    key = open("oppgave7.key", "r").read
-    oppg7 = open("oppgave7.save", "r").read
-    oppg7raw = decrypt(oppg7, key)
-    print(oppg7raw)
-
-
-
-def gen_key():
-    file = open("oppgave7.key", "wb")
-    file.write(Fernet.generate_key())
-
-def word_count(text: str):
-    strings = text.split(" ")
-    for word in strings:
-        pass
     
+def word_count(text: str):
+    ls = text.split("\n")
+    
+    tmp_ordliste = {}
+    for i in ls:
+        if i.lower() not in tmp_ordliste:
+            tmp_ordliste[i.lower()] = 1
+        else:
+            tmp_ordliste[i.lower()] += 1
+    print(tmp_ordliste)
+        
 
-# Generate key
-gen_key()
 # Run until stopped.
 while True:
     # Take input from the user and assign it to the "i" variable.
@@ -53,4 +32,5 @@ while True:
     # Use the write to file functon to save the users supercool words.
     write_to_file(i)
 
-get_file_content()
+word_count(open("oppgave7.save").read())
+

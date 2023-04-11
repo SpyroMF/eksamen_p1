c8b91ac651c18998945d1ab49fee474396e13c27
klype <mikkelfossum@gmail.com>
2023-04-11 11:17:22 +0200

    encryption working (and oppg6 done)

--- py/oppg6/oppgave6.py
+++ py/oppg6/oppgave6.py
@@ -1,9 +1,30 @@
-from cryptography.fernet import Fernet
-
 class Person():
-    def __init__(self, name, age, address) -> None:
+    def __init__(self, name: str, age: int, address: str) -> None:
         self.name = name
         self.age = age
         self.address = address
+    
+    # Denne funksjonen forventer en streng som er enten name, age eller address.
+    # Den vil returnere verdien som skrives inn i get() funksjonen.
+    def get(self, select: str) -> None:
+        if select.lower() == "name":
+            return self.name
+        elif select.lower() == "age":
+            return self.age
+        elif select.lower() == "address":
+            return self.address
+        else:
+            return "ERROR"
+        
+    def change_address(self, address: str):
+        self.address = address
 
-person1 = Person("Kul kar", "18", "2 Kult,Sted 2001")
+person1 = Person("Kul kar", 18, "2 Kult,Sted 2001")
+
+print("Navn:      ", person1.get("name"))
+print("Alder:     ", person1.get("age"))
+print("Addresse:  ", person1.get("address"))
+
+person1.change_address("19 Ikke Kult,Sted 3012")
+
+print("Addresse2: ", person1.get("address"))
--- py/oppg7/oppgave7.py
+++ py/oppg7/oppgave7.py
@@ -1,17 +1,41 @@
+from cryptography.fernet import Fernet
+
+# thanks https://stackoverflow.com/questions/2490334/simple-way-to-encode-a-string-according-to-a-password
+def encrypt(message: bytes, key: bytes) -> bytes:
+    return Fernet(key).encrypt(message)
+
+def decrypt(token: bytes, key: bytes) -> bytes:
+    return Fernet(key).decrypt(token)
+
 # This function takes in a string, then opens
 # a file, then writes the string to it.
 # ----------------------------------------------
 # IF THE FILE DOES NOT WORK IT IS IMPORTANT TO 
 # RUN IT WITH THE PY/PYTHON COMMAND IN THE FOLDER IT IS.
 def write_to_file(string: str):
-    file = open("oppgave7.save", "a") # Opens a file as an variable
-    file.write(string+"\n") # Writes the choosen string to the file
+    file = open("oppgave7.save", "ab") # Opens a file as an variable
+
+    key = open("oppgave7.key", "r").read()
+    print("Key:",key) # Debug
+    token = encrypt(bytes(string, encoding="utf8"), key)
+    file.write(token+"\n".encode()) # Writes the choosen string to the file
     file.close # Closes the file
-    
+
+
+
+
+def gen_key():
+    file = open("oppgave7.key", "wb")
+    file.write(Fernet.generate_key())
+
 def word_count(text: str):
     strings = text.split(" ")
+    for word in strings:
+        pass
     
 
+# Generate key
+gen_key()
 # Run until stopped.
 while True:
     # Take input from the user and assign it to the "i" variable.

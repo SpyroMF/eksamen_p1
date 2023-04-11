cf589d7047bfb8fab51aafa8ca7c51c6cad6d7a7
klype <mikkelfossum@gmail.com>
2023-04-11 09:35:21 +0200

    added various functions to the Rektangel() class

--- py/oppg5/oppgave5.py
+++ py/oppg5/oppgave5.py
@@ -1,4 +1,14 @@
 class Rektangel():
-    def __init__(self, lenght, width) -> None:
-        self.length = lenght
-        self.width = width
+    def __init__(self, lenght: int, width: int) -> None:
+        self.length: int = lenght
+        self.width: int = width
+    def get_area(self):
+        return self.length * self.width
+    def get_circumference(self):
+        return self.length + self.width
+
+rektangel1 = Rektangel(10, 23)
+print(
+    "Rektangel areal:   ",rektangel1.get_area(),"\n"+
+    "Rektangel omkrets: ",rektangel1.get_circumference()
+)

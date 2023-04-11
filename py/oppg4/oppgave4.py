53667d2324190e8b9d32d17cfe7d3d8b7d6aa8e5
klype <mikkelfossum@gmail.com>
2023-04-11 09:26:43 +0200

    added usage for the Elev() class

--- py/oppg4/oppgave4.py
+++ py/oppg4/oppgave4.py
@@ -1,7 +1,18 @@
 class Elev():
-    def __init__(self, name, address, age, phonenumber, email) -> None:
-        self.name = name
-        self.adddress = address
-        self.age = age
-        self.phonenumber = phonenumber
-        self.email = email
+    def __init__(self, name: str, address: str, age: int, phonenumber: str, email: str) -> None:
+        self.name: str = name
+        self.address: str = address
+        self.age: int = age
+        self.phonenumber: str = phonenumber
+        self.email: str = email
+    def print_info(self):
+        print("Navn:    ", self.name)
+        print("Addresse:", self.address)
+        print("Alder:   ", self.age)
+        print("Tlf:     ", self.phonenumber)
+        print("E-post:  ", self.email)
+
+
+#Eksempel
+testperson: Elev = Elev("Kul Kar","2 Kult,Sted 2001", 18, "942 44 544", "kulkar91@gmail.com")
+testperson.print_info()
